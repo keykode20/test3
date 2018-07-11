1e6b6ab87e38dfcb08955daa28e2ff4d21d910e8
omry <omry@codegen.com>
2018-07-11 01:02:06 +0100

    Worked fine

---
+++ ScenarioCustomerplaceanorderbypurchasinganitemfromsearch.java
@@ -0,0 +1,41 @@
+
+package my.code.generated;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+
+
+/**
+ * My example.
+ * 
+ */
+public class ScenarioCustomerplaceanorderbypurchasinganitemfromsearch {
+
+    @Given("Given user is on Home PageWhen he search for dress\r")
+    public void givenuserisonhomepagewhenhesearchfordress() {
+    }
+
+    @And("And choose to buy the first item\r")
+    public void andchoosetobuythefirstitem() {
+    }
+
+    @And("And moves to checkout from mini cart\r")
+    public void andmovestocheckoutfromminicart() {
+    }
+
+    @And("And enter personal details on checkout page\r")
+    public void andenterpersonaldetailsoncheckoutpage() {
+    }
+
+    @And("And select same delivery address\r")
+    public void andselectsamedeliveryaddress() {
+    }
+
+    @And("And select payment method as check payment\r")
+    public void andselectpaymentmethodascheckpayment() {
+    }
+
+    @And("And place the order")
+    public void andplacetheorder() {
+    }
+}
